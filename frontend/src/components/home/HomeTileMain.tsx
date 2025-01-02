af4f7725de5a983daf1417f1564b7b5c34ea4f7e
Viktor Bezai <viktorbezai@gmail.com>
2025-01-01 22:07:56 -0500

    90 - HomeTileMain improved

--- frontend/src/components/home/HomeTileMain.tsx
+++ frontend/src/components/home/HomeTileMain.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import {Box, Typography} from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 interface HomeTileMainProps {
   title: string;
@@ -8,33 +8,43 @@ interface HomeTileMainProps {
   link: string;
 }
 
-const HomeTileMain = ({title, description, image, link}: HomeTileMainProps) => {
-
+const HomeTileMain = ({ title, description, image, link }: HomeTileMainProps) => {
   return (
     <Link href={link} passHref>
       <Box
         sx={{
           mx: "auto",
           position: "relative",
-          minHeight: {xs: 350, md: 450, lg: 500, xl: 700},
-          minWidth: {md: 150, xl: 250},
-          borderRadius: 1,
+          minHeight: { xs: 300, md: 450, lg: 500, xl: 700 },
+          minWidth: { md: 150, xl: 250 },
+          borderRadius: {xs: 2, md: 1},
           overflow: "hidden",
           backgroundImage: `url(${image})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           cursor: "pointer",
           transition: "filter 0.3s, background-color 0.3s",
-          filter: {md: "brightness(0.5)"},
+          filter: { md: "brightness(0.5)" },
           "&:hover": {
             filter: "brightness(1)",
+          },
+          "&::after": {
+            content: '""',
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: { xs: "rgba(0, 0, 0, 0.8)", md: "transparent" },
+            zIndex: 1,
+            pointerEvents: "none",
           },
         }}
       >
         <Box
           sx={{
             position: "absolute",
-            p: 2,
+            p: {xs: 1, md: 2},
             top: 0,
             left: 0,
             right: 0,
@@ -42,6 +52,7 @@ const HomeTileMain = ({title, description, image, link}: HomeTileMainProps) => {
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
+            zIndex: 2, // Ensures the text appears above the overlay
           }}
         >
           <Typography
@@ -49,7 +60,13 @@ const HomeTileMain = ({title, description, image, link}: HomeTileMainProps) => {
               color: "white",
               fontWeight: "bold",
               textAlign: "center",
-              fontSize: {xs: "1.1rem", sm: "1rem", md: "0.7rem", lg: "0.9rem", xl: "1.25rem"}
+              fontSize: {
+                xs: "1.1rem",
+                sm: "1rem",
+                md: "0.7rem",
+                lg: "0.9rem",
+                xl: "1.25rem",
+              },
             }}
           >
             {title}
@@ -65,11 +82,12 @@ const HomeTileMain = ({title, description, image, link}: HomeTileMainProps) => {
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
+            zIndex: 2, // Ensures the text appears above the overlay
           }}
         >
           <Box
             sx={{
-              backgroundColor: "rgba(0, 0, 0, 0.7)",
+              backgroundColor: {xs: "none", md: "rgba(0, 0, 0, 0.7)"},
               p: 2,
             }}
           >
@@ -77,7 +95,7 @@ const HomeTileMain = ({title, description, image, link}: HomeTileMainProps) => {
               sx={{
                 color: "white",
                 textAlign: "center",
-                fontSize: {xs: "0.9rem", sm: "0.75rem", lg: "0.9rem", xl: "1rem"}
+                fontSize: { xs: "0.9rem", sm: "0.75rem", lg: "0.9rem", xl: "1rem" },
               }}
             >
               {description}
@@ -86,7 +104,7 @@ const HomeTileMain = ({title, description, image, link}: HomeTileMainProps) => {
         </Box>
       </Box>
     </Link>
-  )
+  );
 };
 
 export default HomeTileMain;
